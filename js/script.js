a69be0c92e77e90e41068c3237d7558abf7503f5
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-03-03 09:49:39 +0200

    some fix

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ class Employee extends Person {
     constructor(firstName, lastName, age, birthDayDate, jobPosition) {
         super(firstName, lastName, age, birthDayDate);
         this.jobPosition = jobPosition;
+        this.birthDayDate = birthDayDate;
     }
 
     #salary = 10000;
@@ -27,24 +28,34 @@ class Employee extends Person {
         return `Моя годовая зарплата ${myYearSalary}$!`;
     }
 
+    // celebrate() {
+    //     const today = new Date();
+    //     const day = (new Date(today.getFullYear(), this.birthDayDate.getMonth(), this.birthDayDate.getDate())).getDay();
+    //     if (day === 0 || day === 6) {
+    //         return "Happy Birthday, lets celebrate!";
+    //     } else {
+    //         return "Happy Birthday, but I need to work!";
+    //     }
+    // }
+
     celebrate() {
-        const today = new Date();
-        const day = (new Date(today.getFullYear(), this.birthDayDate.getMonth(), this.birthDayDate.getDate())).getDay();
+        const day = (this.birthDayDate.setFullYear()).getDay();
         if (day === 0 || day === 6) {
             return "Happy Birthday, lets celebrate!";
         } else {
             return "Happy Birthday, but I need to work!";
         }
     }
+
 }
 
 
-const user1 = new Person("John", "Snow", 25, "1993-03-12");
+const user1 = new Person("John", "Snow", 25, "1993-12-12");
 console.log(user1.celebrate());
 console.log(user1.birthDayDate);
 
-const user2 = new Employee("Mike", "Brown", 35, "1993-04-13", "driver");
-console.log(user2);
+const user2 = new Employee("Mike", "Brown", 35, "1993-10-10", "driver");
+console.log(user2.birthDayDate);
 console.log(user2.celebrate());
 
 
@@ -53,4 +64,7 @@ console.log(user2.celebrate());
 
 
 
-// const day = (new Date(today.getFullYear(), this.birthDayDate.getMonth(), this.birthDayDate.getDate())).getDay();
+
+
+
+
